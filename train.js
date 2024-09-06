9bb720b3f35470666ab3aeedfb340856e2dd171e
BahodirL7 <bakhodirtursunovl7@gmail.com>
2024-09-06 10:14:49 +0900

    feat: Refactoring H-task

--- train.js
+++ train.js
@@ -1,3 +1,16 @@
+// H-TASK:
+
+// Shunday function tuzing, u integerlardan iborat arrayni argument sifatida qabul qilib, faqat positive qiymatlarni olib string holatda return qilsin
+// MASALAN: getPositive([1, -4, 2]) return qiladi "12"
+
+function getPositive(arr) {
+  const total = arr.filter((num) => num > 0).join("");
+  return total;
+}
+
+const result = getPositive([1, -4, 2]);
+console.log("H-TASK:", result);
+
 // G-TASK:
 
 /*
@@ -5,11 +18,11 @@ Shunday function tuzingki unga integerlardan iborat array pass bolsin va functio
 MASALAN: getHighestIndex([5, 21, 12, 21, 8]) return qiladi 1 sonini.
 */
 
-function getHighestIndex(arr) {
-  let maxValue = Math.max(...arr);
-  return arr.indexOf(maxValue);
-}
+// function getHighestIndex(arr) {
+//   let maxValue = Math.max(...arr);
+//   return arr.indexOf(maxValue);
+// }
 
-let array = [7, 11, 12, 22, 8];
-let result = getHighestIndex(array);
-console.log("G-TASK:", result);
+// let array = [7, 11, 12, 22, 8];
+// let result = getHighestIndex(array);
+// console.log("G-TASK:", result);
