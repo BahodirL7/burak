c260b3fab33c2e21fe01593df80d82251d3204be
BahodirL7 <bakhodirtursunovl7@gmail.com>
2024-09-06 18:39:30 +0900

    feat: connect mongoDB via mongoose

--- src/server.ts
+++ src/server.ts
@@ -7,8 +7,19 @@
 
 import dotenv from 'dotenv';
 dotenv.config();
+console.log(dotenv.config());
 
 console.log("PORT:", process.env.PORT);
 
 // MongoDB == CLUSTER => DATABASE => COLLECTION => DOCUMENT = DISTRIBUTED DATABASE (DB);
-// MySQL == CLUSTER => DATABASE => TABLE => DATA = RELATIONAL DATABASE
+// MySQL == CLUSTER => DATABASE => TABLE => DATA = RELATIONAL DATABASE;
+
+import mongoose from 'mongoose';
+
+mongoose.connect(process.env.MONGO_URL as string, {})
+.then((data) => 
+    {console.log("MongoDB connection succeed")
+    const PORT = process.env.PORT ?? 3003;
+})
+.catch((err) => console.log("Error on connection to MongoDB", err)
+);
