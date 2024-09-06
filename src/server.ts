b24f2b57e62e82d15b5fb952f675164874b2c9bd
BahodirL7 <bakhodirtursunovl7@gmail.com>
2024-09-06 19:05:43 +0900

    feat: express integrations

--- src/server.ts
+++ src/server.ts
@@ -1,25 +1,33 @@
-// Architectural pattern: MVC, DI, MVP
+/*
+Architectural pattern: MVC, DI, MVP
 
-// MVC = MODAL, VIEW, CONTROLLER
-// DI = DEPENDENCY INJECTION
+MVC = MODAL, VIEW, CONTROLLER
+DI = DEPENDENCY INJECTION
 
-// Design pattern: Middleware, Decorator
+Design pattern: Middleware, Decorator
+*/
+
+/*
+MongoDB == CLUSTER => DATABASE => COLLECTION => DOCUMENT = DISTRIBUTED DATABASE (DB);
+MySQL == CLUSTER => DATABASE => TABLE => DATA = RELATIONAL DATABASE;
+*/
 
 import dotenv from 'dotenv';
 dotenv.config();
 console.log(dotenv.config());
-
 console.log("PORT:", process.env.PORT);
 
-// MongoDB == CLUSTER => DATABASE => COLLECTION => DOCUMENT = DISTRIBUTED DATABASE (DB);
-// MySQL == CLUSTER => DATABASE => TABLE => DATA = RELATIONAL DATABASE;
-
 import mongoose from 'mongoose';
+import app from "./app"
 
-mongoose.connect(process.env.MONGO_URL as string, {})
-.then((data) => 
-    {console.log("MongoDB connection succeed")
+mongoose
+.connect(process.env.MONGO_URL as string, {})
+.then((data) => {
+    console.log("MongoDB connection succeed")
     const PORT = process.env.PORT ?? 3003;
+    app.listen(PORT, function(){
+        console.log(`The server is running successfully on port: ${PORT}`);
+    })
 })
 .catch((err) => console.log("Error on connection to MongoDB", err)
 );
