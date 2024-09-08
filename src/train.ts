4975c9a408f78b0108ff27cb0b2f77d16b4f6c96
BahodirL7 <bakhodirtursunovl7@gmail.com>
2024-09-08 15:18:14 +0900

    feat: Refactoring H2-Task

--- src/train.ts
+++ src/train.ts
@@ -1,15 +1,33 @@
+// H2-TASK: 
+
+/*
+Shunday function tuzing, unga string argument pass bolsin. Function ushbu agrumentdagi digitlarni yangi stringda return qilsin
+MASALAN: getDigits("m14i1t") return qiladi "141"
+*/
+function getDigits(str: string) {
+  let digits = '';
+  for (let s of str) {
+    if(!isNaN(Number(s))) {
+      digits += s;
+    }
+  } return digits;
+}
+
+const result = getDigits("m14i71t")
+console.log("H2-TASK:", result);
+
 // H-TASK:
 
 // Shunday function tuzing, u integerlardan iborat arrayni argument sifatida qabul qilib, faqat positive qiymatlarni olib string holatda return qilsin
 // MASALAN: getPositive([1, -4, 2]) return qiladi "12"
 
-function getPositive(arr: number[]): string {
-  const total = arr.filter((num: number) => num > 0).join("");
-  return total;
-}
+// function getPositive(arr: number[]): string {
+//   const total = arr.filter((num: number) => num > 0).join("");
+//   return total;
+// }
 
-const result = getPositive([1, -4, 2]);
-console.log("H-TASK:", result);
+// const result = getPositive([1, -4, 2]);
+// console.log("H-TASK:", result);
 
 // G-TASK:
 
