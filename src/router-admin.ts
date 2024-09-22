1fa94738dce73b292c899001b9cdc740c7a2604e
BahodirL7 <bakhodirtursunovl7@gmail.com>
2024-09-23 00:28:07 +0900

    feat: upload files to server via multer

--- src/router-admin.ts
+++ src/router-admin.ts
@@ -2,6 +2,7 @@ import express from "express";
 const routerAdmin = express.Router();
 import restaurantController from "./controllers/restaurant.controller";
 import productController from "./controllers/product.controller";
+import makeUploader from "./libs/utils/uploader";
 
 routerAdmin.get("/", restaurantController.goHome);
 
@@ -11,7 +12,7 @@ routerAdmin
  
 routerAdmin
 .get("/signup", restaurantController.getSignup)
-.post("/signup", restaurantController.processSignup);
+.post("/signup", makeUploader("members").single("memberImage"), restaurantController.processSignup);
 
 routerAdmin.get("/logout", restaurantController.logout);
 routerAdmin.get("/check-me", restaurantController.checkAuthSession);
@@ -22,11 +23,13 @@ routerAdmin.get(
     restaurantController.verifyRestaurant, 
     productController.getAllProducts
 );
-
+  
 routerAdmin.post(
-    "/product/create",
+    "/product/create", 
     restaurantController.verifyRestaurant,
-    productController.createNewProduct
+    // uploadProductImage.single("productImage"),
+    makeUploader("products").array("productImages", 5),
+    productController.createNewProduct,
 );
 
 routerAdmin.post(
