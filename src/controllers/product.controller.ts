7e2d5384bfad7e31bd0ca0ff57ef299d57df4fb2
BahodirL7 <bakhodirtursunovl7@gmail.com>
2024-09-25 17:46:11 +0900

    feat: createNewProduct business logic by restaurant member

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -1,11 +1,18 @@
 import { Request, Response } from "express";
-import Errors from "../libs/Errors";
+import Errors, { HttpCode, Message } from "../libs/Errors";
 import { T } from "../libs/types/common";
 import ProductService from "../models/product.service";
+import { ProductInput } from "../libs/types/product";
+import { AdminRequest } from "../libs/types/member";
 
 const productService = new ProductService();
 
 const productController: T = {};
+
+/** SPA */
+
+/** SSR */
+
 productController.getAllProducts = async (req: Request, res: Response) => {
     try {
         console.log('getAllProducts');
@@ -17,14 +24,29 @@ productController.getAllProducts = async (req: Request, res: Response) => {
     }
 };
 
-productController.createNewProduct = async (req: Request, res: Response) => {
+productController.createNewProduct = async (req: AdminRequest, res: Response) => {
     try {
         console.log('createNewProduct');
-        res.send("DONE")
+        console.log(req.files);
+        if (!req.files?.length)
+            throw new Errors(HttpCode.INTERNAL_SERVER_ERROR, Message.CREATE_FAILED);
+
+        const data: ProductInput = req.body;
+        data.productImages = req.files?.map((ele) => {
+            return ele.path;
+        });
+
+        // console.log("data:", data);
+
+        await productService.createNewProduct(data);
+
+        res.send(`<script> alert("Successful Creation!"); window.location.replace('admin/product/all') </script>`);
+
     } catch (error) {
         console.log("Error, createNewProduct", error);
-        if (error instanceof Errors) res.status(error.code).json(error);
-        else res.status(Errors.standart.code).json(Errors.standart);
+        const message = error instanceof Errors ? error.message : Message.SOMETHING_WENT_WRONG;
+
+        res.send(`<script> alert(${message}); window.location.replace('admin/product/all') </script>`);
     }
 };
 
